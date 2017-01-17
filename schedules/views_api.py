9bc358cc953a87b2b3aaa74884fd112107e85643
Kevin Schellenberg <wikkiewikkie@gmail.com>
2017-01-16 21:09:55 -0500

    add tournament api view

--- schedules/views_api.py
+++ schedules/views_api.py
@@ -1,7 +1,9 @@
-from schedules import app
-from schedules.models import Division, Team
+from schedules import app, db
+from schedules.models import Division, Team, Tournament
 
-from flask import jsonify
+from flask import jsonify, request
+
+import datetime
 
 
 @app.route("/api/tournaments/<tournament_id>/divisions")
@@ -49,3 +51,28 @@ def teams(tournament_id):
         team['matches'] = r.matches
         teams.append(team)
     return jsonify({'teams': teams})
+
+
+@app.route("/api/tournaments", methods=['GET', 'POST'])
+def tournaments():
+    if request.method == 'POST':
+        t = Tournament(
+            name=request.form['name'],
+            start_date=datetime.datetime.strptime(request.form['start_date'], '%Y-%m-%d'),
+            end_date=datetime.datetime.strptime(request.form['end_date'], '%Y-%m-%d')
+        )
+        db.session.add(t)
+        db.session.commit()
+
+
+    result = Tournament.query.all()
+    ts = []
+    for r in result:
+        t = dict()
+        t['id'] = r.id
+        t['name'] = r.name
+        t['start_date'] = r.start_date
+        t['end_date'] = r.end_date
+        ts.append(t)
+
+    return jsonify({'tournaments': ts})
